c5efe872204453c60af454de5af617951dc9c11d
hcs <hannahemcs@gmail.com>
2023-11-06 15:28:08 +0100

    Remove sleep function

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -12,15 +12,10 @@ public class Customer implements Runnable {
     public void run() {
         Scanner scanner = new Scanner(System.in);
         Thread depositThread = new Thread(() -> {
-            try {
                 System.out.println("Enter deposit amount");
                 double deposit = scanner.nextDouble();
                 System.out.println("Processing deposit...");
-                Thread.sleep(1000);
                 bankAccount.deposit(deposit);
-            } catch (InterruptedException e) {
-                System.err.println("Thread was interrupted");
-            }
         });
 
         depositThread.start();
