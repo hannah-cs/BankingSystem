78512b0ff762baf5f74adb37f5ecd1b068d8c9eb
hcs <hannahemcs@gmail.com>
2023-11-06 14:46:03 +0100

    Exercise 2

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,31 @@
+package org.example;
+import java.util.Scanner;
+
+public class Customer implements Runnable {
+        public int balance;
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+        Thread depositThread = new Thread(() -> {
+            try {
+                System.out.println("Enter deposit amount");
+                int deposit = scanner.nextInt();
+                balance =+ deposit;
+                System.out.println("Processing deposit...");
+                Thread.sleep(1000);
+                System.out.println("Deposit successful. New balance: €"+balance);
+            } catch (InterruptedException e) {
+                System.err.println("Thread was interrupted");
+            }
+        });
+            depositThread.start();
+    }
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Thread customerThread = new Thread(customer);
+        customerThread.start();
+        Customer customer2 = new Customer();
+        Thread customerThread2 = new Thread(customer2);
+        customerThread2.start();
+    }
+}
