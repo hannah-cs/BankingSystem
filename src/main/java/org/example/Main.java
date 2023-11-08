f76c3deca870eee14ab654d6308b58582e12b728
hcs <hannahemcs@gmail.com>
2023-11-08 14:29:42 +0100

    Manage threads with executorservice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
         BankAccount sharedAccount = new BankAccount(1234);
         Customer customer = new Customer(sharedAccount);
-        Thread customerThread = new Thread(customer);
         Customer customer2 = new Customer(sharedAccount);
-        Thread customerThread2 = new Thread(customer2);
-        customerThread.start();
-        customerThread2.start();
+        executor.submit(customer);
+        executor.submit(customer2);
+        executor.shutdown();
     }
 }
