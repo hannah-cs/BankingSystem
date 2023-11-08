d3cb29813c0b77569e796f61cfd3877cbb19f870
hcs <hannahemcs@gmail.com>
2023-11-08 16:25:58 +0100

    Future

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,28 +1,39 @@
 package org.example;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
+
 
 public class BankAccount {
     public static List<BankAccount> allAccounts = new ArrayList<>();
     private double balance;
     private int id;
+    private ExecutorService executor;
 
     public BankAccount(int id) {
         this.id = id;
         allAccounts.add(this);
+        executor = Executors.newCachedThreadPool();
+    }
+    public Future<Double> deposit(double depositAmount) {
+        return executor.submit(() -> {
+            balance = balance + depositAmount;
+            System.out.println("Deposited €" + depositAmount + ". New balance €" + balance);
+            return balance;
+        });
     }
 
-    public void deposit(double depositAmount) {
-        balance = balance + depositAmount;
-        System.out.println("Deposited €"+depositAmount+". New balance €"+balance);
-    }
 
-    public void withdraw(double withdrawalAmount){
-        if ((balance-withdrawalAmount) >= 0){
-            balance = balance - withdrawalAmount;
-            System.out.println("Withdrawal successful. New balance €"+balance);
-        } else {
-            System.out.println("Insufficient funds.");
-        }
+    public Future<Double> withdraw(double withdrawalAmount) {
+        return executor.submit(() -> {
+            if ((balance - withdrawalAmount) >= 0) {
+                balance = balance - withdrawalAmount;
+                System.out.println("Withdrawal successful. New balance €" + balance);
+                return balance;
+            } else {
+                System.out.println("Insufficient funds.");
+                return -1.0;
+            }
+        });
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
-import java.util.concurrent.*;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newCachedThreadPool();
         BankAccount sharedAccount = new BankAccount(1234);
+        Scanner scanner = new Scanner(System.in);
         Customer customer = new Customer(sharedAccount);
         Customer customer2 = new Customer(sharedAccount);
         executor.submit(customer);
